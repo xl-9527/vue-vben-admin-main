212bbb1ea213930b9659a2f4c4924b1fc95c64c0
xl-9527 <xl-9527@>
2025-08-07 19:03:54 +0800

    路由初始化，项目初始化

--- apps/web-antd/src/router/routes/modules/cdata.ts
+++ apps/web-antd/src/router/routes/modules/cdata.ts
@@ -8,18 +8,45 @@ const routes: RouteRecordRaw[] = [
       icon: 'ic:baseline-view-in-ar',
       keepAlive: true,
       order: 1000,
-      title: $t('demos.title'),
+      title: $t('cdata.title'),
     },
-    name: 'Demos',
-    path: '/demos',
+    name: 'Cdata',
+    path: '/cdata',
     children: [
       {
         meta: {
-          title: $t('demos.antd'),
+          title: $t('cdata.expend.title'),
         },
-        name: 'AntDesignDemos',
-        path: '/demos/ant-design',
-        component: () => import('#/views/demos/antd/index.vue'),
+        name: 'CdataExpend',
+        path: '/cdata/expend',
+        children: [
+          {
+            meta: {
+              title: $t('cdata.expend.bill.title'),
+            },
+            name: 'CdataExpendBill',
+            path: '/cdata/expend/bill',
+            component: () => import('#/views/cdata/expends/bill/index.vue'),
+          },
+          {
+            meta: {
+              title: $t('cdata.expend.account_book.title'),
+            },
+            name: 'CdataExpendAccountBook',
+            path: '/cdata/expend/account_book',
+            component: () =>
+              import('#/views/cdata/expends/account_book/index.vue'),
+          },
+          {
+            meta: {
+              title: $t('cdata.expend.bill_plan.title'),
+            },
+            name: 'CdataExpendBillPlan',
+            path: '/cdata/expend/bill_plan',
+            component: () =>
+              import('#/views/cdata/expends/bill_plan/index.vue'),
+          },
+        ],
       },
     ],
   },
